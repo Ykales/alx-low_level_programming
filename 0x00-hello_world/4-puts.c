f857b6f66b7fd8174db51157a0ab76a56ead7e97
ajibola oluyinka <oluyinka.ajibola84@gmail.com>
2022-10-13 17:06:08 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 /**
- *  main -Entry point
- * 
- *  Return: Always 0 (Success)
- *
+ * main - Entry ponit
+ * Return: 0 (for success)
  */
 int main(void)
 {
-		puts("Programming is like building a multilingual puzzle/n");
-		return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
