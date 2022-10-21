85e39eb45e43a96b36d061e43060a240765b73a2
ajibola oluyinka <oluyinka.ajibola84@gmail.com>
2022-10-21 06:09:00 -0700

    tsk

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,16 +4,25 @@
  * return 0
  */
 int main(void)
-
 {
-	putchar('_');                                                                                                                                   
-       _putchar('p');                                                                                                                                   
-       _putchar('u');                                                                                                                                   
-       _putchar('t');                                                                                                                                   
-       _putchar('c');                                                                                                                                   
-       _putchar('h');                                                                                                                                   
-       _putchar('a');                                                                                                                                   
-       _putchar('r');                                                                                                                                   
-       _putchar('\n');                                                                                                                                  
-       Return (0);
-}     
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_puychar('a');
+_putchar('r');
+_putchar('\n');
+Return (0);
+}
+                                                                                                           
+                                                                                             
+                                                                                                           
+                                                                                                          
+                                                                                                            
+                                                                                                          
+                                                                                                              
+                                                                                                               
+
+  
