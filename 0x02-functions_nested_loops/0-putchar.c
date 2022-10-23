d7cdc7c0eed6381a8ed716c77ac1004100159b60
ajibola oluyinka <oluyinka.ajibola84@gmail.com>
2022-10-23 09:24:16 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,37 +1,20 @@
 #include "main.h"
 /**
-<<<<<<< HEAD
  * main - Print _putchar
  * Return: 0 always
  */
-=======
-  Main - Print _putchar
-	Return: 0
-*/
->>>>>>> a072615254ccde722f31e6a80c8fbddf7f23c021
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
 }
-<<<<<<< HEAD
-                                                                                             
-                                                                                                           
-                                                                                                          
-                                                                                                            
-                                                                                                          
-                                                                                                              
-                                                                                                               
-
-  
-=======
 
 
 
@@ -40,4 +23,15 @@ return (0);
 
 
 
->>>>>>> a072615254ccde722f31e6a80c8fbddf7f23c021
+
+
+
+
+
+
+
+
+
+
+
+
