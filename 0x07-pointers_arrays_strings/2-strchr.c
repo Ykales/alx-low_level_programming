21da3e9e09c0a86027c462c4f19f175038c16e32
ajibola oluyinka <oluyinka.ajibola84@gmail.com>
2022-10-31 10:28:36 -0700

    tast 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,28 +1,26 @@
 #include "main.h"
-  2
-  3 /**
-  4  * _strchr - locates a character in a string
-  5  *
-  6  * @c: occurrence of the character
-  7  *
-  8  * @s: in the string
-  9  *
- 10  * Return: a pointer to the first occurrence of the character
- 11  */
- 12
- 13 char *_strchr(char *s, char c)
- 14 {
- 15         while (*s)
- 16         {
- 17                 if (*s != c)
- 18                         s++;
- 19                 else
- 20                         return (s);
- 21         }
- 22         if (c == '\0')
- 23                 return (s);
- 24
- 25         return (NULL);
- 26 }
 
+/**
+ * _strchr - locates a character in a string
+ *
+ * @c: occurrence of the character
+ *
+ * @s: in the string
+ *
+ * Return: a pointer to the first occurrence of the character
+ */
 
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+	if (c == '\0')
+		return (s);
+
+	return (NULL);
+}
